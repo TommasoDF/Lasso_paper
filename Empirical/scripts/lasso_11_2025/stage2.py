d4ee63f34eb1060dcf12920fdf2b63b3b6b228a3
TommasoDF <tommaso.dif@gmail.com>
2025-12-02 13:30:20 +0100

    correcting_stage1

--- Empirical/scripts/lasso_11_2025/stage2.py
+++ Empirical/scripts/lasso_11_2025/stage2.py
@@ -14,14 +14,14 @@ def estimate_kappa_curve_fit(stage2):
     pcov : 2D array
         Covariance matrix of parameters
     """
-    r = stage2['vwretd'].values[1:]
-    pred_t = stage2['predictions'].values[:-1]
-    pred_t1 = stage2['predictions'].values[1:]
+    r = stage2['vwretd'].values[:-1] # up to r_{t-1}
+    pred_t = stage2['predictions'].values[:-1] # up to r^e_{t-1}
+    pred_t1 = stage2['predictions'].values[1:] # up to r^e_{t}
     
     def alm_model(x, kappa, intercept):
         pred_t, pred_t1 = x
-        # return np.log(1 - kappa * np.exp(pred_t)) - np.log(1 - kappa * np.exp(pred_t1)) + intercept
-        return np.log(1 - kappa * np.exp(- 1/kappa *pred_t * (1 -kappa))) - np.log(1 - kappa * np.exp(- 1/kappa *pred_t1 * (1 -kappa))) + intercept
+        return np.log(1 - kappa * np.exp(pred_t)) - np.log(1 - kappa * np.exp(pred_t1)) + intercept
+        #return np.log(1 - kappa * np.exp(- 1/kappa *pred_t * (1 -kappa))) - np.log(1 - kappa * np.exp(- 1/kappa *pred_t1 * (1 -kappa))) + intercept
     
     try:
         return curve_fit(
@@ -39,23 +39,23 @@ def compute_alm_returns(predictions, kappa, intercept):
     pred_t, pred_t1 = predictions[:-1], predictions[1:]
 
     # validity mask
-    #valid = (1 - kappa * np.exp(pred_t) > 0) & (1 - kappa * np.exp(pred_t1) > 0)
-    valid = (1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t) > 0) & (1 - kappa * np.exp(- 1 / kappa * (1 - kappa) *  pred_t1) > 0)
+    valid = (1 - kappa * np.exp(pred_t) > 0) & (1 - kappa * np.exp(pred_t1) > 0)
+    #valid = (1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t) > 0) & (1 - kappa * np.exp(- 1 / kappa * (1 - kappa) *  pred_t1) > 0)
 
     # initialize output array
     alm = np.full(len(pred_t), np.nan)
 
     # compute only valid entries
-    # alm[valid] = (
-    #     np.log(1 - kappa * np.exp(pred_t[valid]))
-    #     - np.log(1 - kappa * np.exp(pred_t1[valid]))
-    #     + intercept
-    # )
     alm[valid] = (
-        np.log(1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t[valid]))
-        - np.log(1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t1[valid]))
+        np.log(1 - kappa * np.exp(pred_t[valid]))
+        - np.log(1 - kappa * np.exp(pred_t1[valid]))
         + intercept
     )
+    # alm[valid] = (
+    #     np.log(1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t[valid]))
+    #     - np.log(1 - kappa * np.exp(- 1 / kappa * (1 - kappa) * pred_t1[valid]))
+    #     + intercept
+    # )
 
     return alm
 
@@ -160,16 +160,19 @@ def compute_stage2_r_squared(stage2_input, min_train_size=100):
             pred_t = stage2_input['predictions'].iloc[t]
             
             # Check validity
-            if (1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t_minus_1) > 0) and \
-               (1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t) > 0):
-                
+            # if (1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t_minus_1) > 0) and \
+            #    (1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t) > 0):
+            
+            if (1 - kappa_train * np.exp(pred_t_minus_1) > 0) and \
+               (1 - kappa_train * np.exp(pred_t) > 0):
+
                 # Compute OOS prediction for return at time t
-                # r_hat_t = (np.log(1 - kappa_train * np.exp(pred_t_minus_1)) - 
-                #           np.log(1 - kappa_train * np.exp(pred_t)) + 
-                #           intercept_train)
-                r_hat_t = (np.log(1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t_minus_1)) - 
-                          np.log(1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t)) + 
+                r_hat_t = (np.log(1 - kappa_train * np.exp(pred_t_minus_1)) - 
+                          np.log(1 - kappa_train * np.exp(pred_t)) + 
                           intercept_train)
+                # r_hat_t = (np.log(1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t_minus_1)) - 
+                #           np.log(1 - kappa_train * np.exp(-1 / kappa_train * (1 - kappa_train) * pred_t)) + 
+                #           intercept_train)
                 
                 oos_predictions.append(r_hat_t)
                 oos_actuals.append(stage2_input['vwretd'].iloc[t])
